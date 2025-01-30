f4699c98b4e41a5f1fcad72efec71b2b6a6a573a
An1one <linolozano75@gmail.com>
2025-01-31 00:08:49 +0100

    update

--- sources/maps/Objects.tsx
+++ sources/maps/Objects.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.1" name="Objects" tilewidth="160" tileheight="160" tilecount="1" columns="0">
+<tileset version="1.10" tiledversion="1.11.1" name="Objects" tilewidth="160" tileheight="160" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../Texture/TX Tree.png" width="160" height="160"/>
+ </tile>
+ <tile id="1">
+  <image source="../Texture/pillar.png" width="32" height="96"/>
  </tile>
 </tileset>
