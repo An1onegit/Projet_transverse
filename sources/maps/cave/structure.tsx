8af5ed6e611682ce6a6919722b5b9a060c2e1ffd
RomainKK <romain.kohnke.ma@gmail.com>
2025-02-07 12:10:43 +0100

    cave map

---
+++ sources/maps/cave/structure.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="structure" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+ <image source="../../Texture/mainlev_build_scaled_2x_pngcrushed.png" width="2048" height="2048"/>
+</tileset>
