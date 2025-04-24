b9511439de1e7a9c3ca471a450e3e902d4364816
RomainKK <romain.kohnke.ma@gmail.com>
2025-04-25 01:16:09 +0200

    Transition map2

---
+++ sources/maps/cave/bg4a.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="bg4a" tilewidth="32" tileheight="32" tilecount="1800" columns="60">
+ <image source="../../Texture/background4a_scaled_2x_pngcrushed.png" width="1920" height="960"/>
+</tileset>
