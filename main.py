939fabe967684715988f8d18bbac8da4608bdfc1
An1one <linolozano75@gmail.com>
2025-05-09 09:34:05 +0200

    update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def Main():
         screen_width, screen_height = info.current_w, info.current_h
 
     # Pygame setup
-    screen = pygame.display.set_mode((screen_width, screen_height))
+    screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
     
     pygame.display.set_caption("Bear's Fishing Empire")
 
